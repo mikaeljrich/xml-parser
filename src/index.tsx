34b3bebdde23a1246a40e5b783a920f806931ab7
mickerich96 <mickerich96@gmail.com>
2020-08-21 14:07:48 +0200

    Forgot some stuff

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MainView from './Components/MainView';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MainView />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
